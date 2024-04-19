16fd0488bb6355202db83945808d2eabeca2f01a
GeniusOctopus <k.white221@outlook.com>
2024-04-19 21:46:17 +0200

    Add project files.

---
+++ src/KtwAutomotiveEngineering.DataAccess/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace KtwAutomotiveEngineering.DataAccess
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/KtwAutomotiveEngineering.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace KtwAutomotiveEngineering.Shared
+{
+    public class Class1
+    {
+
+    }
+}
