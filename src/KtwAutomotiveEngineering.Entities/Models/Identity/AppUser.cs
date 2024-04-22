584cd417428b47b90c0d52fe2f23baa5ed09dd88
GeniusOctopus <k.white221@outlook.com>
2024-04-22 22:52:32 +0200

    feat: refresh token fields in db

--- src/KtwAutomotiveEngineering.Entities/Models/Identity/AppUser.cs
+++ src/KtwAutomotiveEngineering.Entities/Models/Identity/AppUser.cs
@@ -4,7 +4,9 @@ namespace KtwAutomotiveEngineering.Entities.Models.Identity
 {
     public class AppUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
     }
 }
