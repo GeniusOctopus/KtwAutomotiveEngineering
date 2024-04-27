0dfe8817bb650d5b7fb17fd39fcb9be3e4d39474
GeniusOctopus <k.white221@outlook.com>
2024-04-27 17:18:33 +0200

    feat: api responses

---
+++ src/KtwAutomotiveEngineering.Entities/Responses/ApiNotFoundResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace KtwAutomotiveEngineering.Entities.Responses
+{
+    public abstract class ApiNotFoundResponse(string message) : ApiBaseResponse(false)
+    {
+        public string Message { get; set; } = message;
+    }
+}
