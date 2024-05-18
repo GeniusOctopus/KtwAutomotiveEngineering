02cfda0096c0da308c98e3ca5a4754fe8715ccb0
GeniusOctopus <k.white221@outlook.com>
2024-05-18 17:35:55 +0200

    fix: return created worktasks in workday dto

--- src/KtwAutomotiveEngineering.V1.Shared/Dto/WorkingTime/WorkDayDto.cs
+++ src/KtwAutomotiveEngineering.V1.Shared/Dto/WorkingTime/WorkDayDto.cs
@@ -1,4 +1,4 @@
 ﻿namespace KtwAutomotiveEngineering.V1.Shared.Dto.WorkingTime
 {
-    public record WorkDayDto(Guid Id, DateTime? Start, DateTime? End, bool IsDone);
+    public record WorkDayDto(Guid Id, DateTime? Start, DateTime? End, bool IsDone, IEnumerable<WorkTaskDto>? WorkTasks);
 }
